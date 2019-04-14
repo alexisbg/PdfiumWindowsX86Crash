a2f9ebb5be9ccb343ed26dbb13ed26755ad1f7a8
Alexis Bergue <contact@bergue.net>
2019-04-14 11:45:30 +0200

    Add printProcessMemoryInfo function

--- PdfiumWindowsX86Crash/PdfiumWindowsX86Crash.cpp
+++ PdfiumWindowsX86Crash/PdfiumWindowsX86Crash.cpp
@@ -1,27 +1,37 @@
 //#include "pch.h"
-#include <iostream>
 #include <string>
 #include <fpdfview.h>
+#include "PdfiumWindowsX86Crash.h"
+
+#ifdef _WIN32
+#include <windows.h>
+#include <psapi.h>
+#include <intsafe.h>
+#endif
 
 using namespace std;
 
 
 int main()
 {
+	printProcessMemoryInfo();
+
+
 	FPDF_InitLibrary();
 
 
 	// FPDFBitmap_Create
-	cout << endl << "FPDFBitmap_Create" << endl;
+	printf("\nFPDFBitmap_Create\n");
 
 	for (int i = 0; i < 100; i++) {
-		cout << i << " ";
+		printf("%d ", i);
 
 		FPDF_BITMAP bmp = FPDFBitmap_Create(2500, 3500, 1);
 
 		unsigned long errId = FPDF_GetLastError();
 		if (errId) {
-			cout << endl << "FPDFBitmap_Create throws " << to_string(errId) << endl;
+			printf("\n[ERROR] FPDFBitmap_Create throws %lu\n", errId);
+			printProcessMemoryInfo();
 			break;
 		}
 
@@ -30,10 +40,10 @@ int main()
 
 
 	// FPDFBitmap_CreateEx
-	cout << endl << "FPDFBitmap_CreateEx" << endl;
+	printf("\nFPDFBitmap_CreateEx\n");
 
 	for (int i = 0; i < 100; i++) {
-		cout << i << " ";
+		printf("%d ", i);
 
 		int width = 2500;
 		int height = 3500;
@@ -44,7 +54,8 @@ int main()
 
 		unsigned long errId = FPDF_GetLastError();
 		if (errId) {
-			cout << endl << "FPDFBitmap_CreateEx throws " << to_string(errId) << endl;
+			printf("\n[ERROR] FPDFBitmap_CreateEx throws %lu\n", errId);
+			printProcessMemoryInfo();
 			break;
 		}
 
@@ -53,4 +64,21 @@ int main()
 	}
 
 	FPDF_DestroyLibrary();
+}
+
+
+void printProcessMemoryInfo()
+{
+#ifdef _WIN32
+	float MB_DIVISOR = 1000000;
+
+	PROCESS_MEMORY_COUNTERS_EX pmc;
+	GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc));
+
+	printf("Process Virtual Memory: %.2f Mb\n", pmc.PrivateUsage / MB_DIVISOR);
+	printf("Process RAM: %.2f Mb\n", pmc.WorkingSetSize / MB_DIVISOR);
+	printf("Process Peak RAM: %.2f Mb\n", pmc.PeakWorkingSetSize / MB_DIVISOR);
+#else
+	printf("No memory info available for this platform\n");
+#endif
 }
